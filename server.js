ee8d76475cb1763bb5194a7e7fe795a3d1acd91e
gaharoe <153198261+gaharoe@users.noreply.github.com>
2025-08-06 13:17:34 +0700

    firebase config

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+const db = require("./firebase.js");
+
+const dbCollection = db.collection('data'); 
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/users', async(req, res) => {
+    const snapshot = await dbCollection.get();
+    const users = [];
+    snapshot.forEach(data => {
+        users.push({...data.data()});
+    });
+    console.log(users);
+    res.send(users[0]);
+});
+
+app.listen(3000, () => console.log("started"));
