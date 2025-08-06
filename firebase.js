4b557728bacc378333d11edf8a06d3f247e5fc5d
gaharoe <153198261+gaharoe@users.noreply.github.com>
2025-08-06 14:35:41 +0700

    add railway env var 2

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-key.json") || JSON.parse(process.env.FIREBASE_KEY);
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
